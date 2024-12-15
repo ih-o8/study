d134ee00614497f7dcc66487aede116fbfc2fea1
최이화 <ihwa220@naver.com>
2024-12-16 00:16:52 +0900

    241215

---
+++ 241215/10699. 오늘 날짜.py
@@ -0,0 +1,27 @@
+# from datetime import datetime
+# from time import strftime
+#
+# now = datetime.now()
+#
+# print(now)
+# # 2024-12-15 02:22:28.166887
+#
+# # 1.
+# print(str(now)[:10])
+#
+# #2.
+# print(f'{now.year}-{now.month}-{now.day}')
+#
+# #3.
+# print(now.strftime('%Y-%m-%d'))
+#
+# today = datetime.today()
+# print(strftime('%Y-%m-%d'))
+#
+#
+# import datetime
+# now = datetime.datetime.now()
+# print(now.strftime('%Y-%m-%d'))
+
+
+print('2024-12-15')
---
+++ 241215/2741. N 찍기.py
@@ -0,0 +1,11 @@
+# for문 활용
+for i in range(1, int(input())+1):
+    print(i)
+
+#while문 활용
+n = int(input())
+cnt = 1
+while n != 0:
+    print(cnt)
+    n -= 1
+    cnt += 1
